d87e93f25d0770e15034faa7a80a86a1247876c7
gpetingi <101139537+gpetingi@users.noreply.github.com>
2022-08-18 20:01:44 -0300

    conexion del front con el back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PersonaService } from './service/persona.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  providers: [PersonaService]
 })
 export class AppComponent {
   title = 'ProyFinal_Gpetingi';

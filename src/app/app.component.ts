e57ad042ee9cb902213b43c4da2ac324d6ac72c4
gpetingi <101139537+gpetingi@users.noreply.github.com>
2023-06-05 10:47:24 -0300

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { PersonaService } from './service/persona.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
   providers: [PersonaService]
 })
 export class AppComponent {
--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -1,17 +1,29 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { persona } from '../Model/persona.model';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {persona} from '../Model/persona.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class PersonaService {
   URL = 'http://localhost:8080/personas/';
 
-  constructor(private http: HttpClient) { }
+  headers = new HttpHeaders({
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin'
+  });
 
-  public getPersona(): Observable<persona>{
-    return this.http.get<persona>(this.URL+'traer/perfil');
+  constructor(private http: HttpClient) {
+  }
+
+  public getPersona(): Observable<persona> {
+
+
+    return this.http.get<persona>(this.URL + 'traer/perfil/1', {
+      headers: this.headers
+    });
   }
 }
